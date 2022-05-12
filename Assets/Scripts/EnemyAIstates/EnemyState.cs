06f0000b1129a9a25a48223aadd2244f01665b8d
just4fun13 <saruhanyans@gmail.com>
2022-05-12 11:11:36 +0900

    state machine logic begininig

---
+++ Assets/Scripts/EnemyAIstates/EnemyState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.EnemyAIstates
+{
+    public abstract class EnemyState
+    {
+        public virtual void EnterState()
+        {
+
+        }
+         
+        public virtual void ExitState()
+        {
+
+        }
+
+        public virtual void UpdateState()
+        {
+
+        }
+    }
+}
